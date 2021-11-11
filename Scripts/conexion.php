19e7328e498e65bb64a4185b120744456f413294
Asier <aurunuelas@uoc.edu>
2021-11-11 11:49:09 +0100

    conexion.php: Código de Conexión y documentación

--- Scripts/conexion.php
+++ Scripts/conexion.php
@@ -1,5 +1,24 @@
 <?php
 
+/*El objetivo de este archivo sera la configuracion de la conexion a la DB PHPMyAdmin */
+
+$server = "localhost";
+$usuario = "root";
+$pass = "123456";
+$db = "producto2p6";
+
+
+$conexion = new mysqli($server, $usuario, $pass, $db); //1 
+
+if($conexion ->connect_errno){ //2
+
+die("Error de conexión. Revise las credenciales". $conexion ->connect_errno);
+
+}else{
+
+    echo "¡Conexión exitosa!";
+}
+
 
 
 
