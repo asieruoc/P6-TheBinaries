07be52edb8eda4643e02f17d3537a9f8b94b2be6
sfigueroaa <sfigueroaa@uoc.edu>
2021-11-21 15:48:47 +0100

    Index funcionando y JS

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+#Inclumos una y solo una vez el config.php
+//require_once("config.php");
+
+$header  = "view/templates/header.php";
+require($header);
+$content = "view/indexView.php";
+require($content);
+
+
+?>
+
