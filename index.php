311e8e0a7c6934097a88c4d57d86b9d9d85d871f
PakiRV <frodriguezvaz@uoc.edu>
2021-11-21 16:39:19 +0100

    Esquema index.php

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
-<?php 
-#Inclumos una y solo una vez el config.php
-//require_once("config.php");
+<!DOCTYPE html>
+<html lang="es">
+<head>
+</head>
 
-$header  = "view/templates/header.php";
-require($header);
-$content = "view/indexView.php";
-require($content);
+<body>
+</body>
 
-
-?>
-
+</html>
