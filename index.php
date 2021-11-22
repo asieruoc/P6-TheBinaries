a8987d1e3d8a79cf8f354d0f6602e3d904ab35aa
PakiRV <frodriguezvaz@uoc.edu>
2021-11-22 10:35:28 +0100

    Index principal corregido

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-<?php
+<?php 
 
 $header  = "view/templates/header.php";
 require($header);
 $content = "view/indexView.php";
 require($content);
-
+$footer  = "view/templates/footer.php";
+require($footer);
 
 ?>
-
 
