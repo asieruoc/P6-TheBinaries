988849df37286b1671611f15bf86ad756773866c
PakiRV <frodriguezvaz@uoc.edu>
2021-12-26 21:00:59 +0100

    Retoques finales

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,7 +64,7 @@
                     </li>
                     <li class="{{ (request()->is('exams')) ? 'active' : '' }}">
                         <a class="navbar-brand" href="{{ url('/exams') }}">
-                            {{ config('Examanes', 'Examenes') }}
+                            {{ config('Exámenes', 'Exámenes') }}
                         </a>
                     </li>
                     <li class="{{ (request()->is('works')) ? 'active' : '' }}">
@@ -160,7 +160,7 @@
 
                                             <a class="dropdown-item" href="{{ route('logout') }}"
                                                 onclick="event.preventDefault();
-                                                                                                    document.getElementById('logout-form').submit();">
+                                                document.getElementById('logout-form').submit();">
                                                 {{ __('Logout') }}
                                             </a>
 
