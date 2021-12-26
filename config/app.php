3c4b477f4ae296df2ea4ca4b999e90d8fe0f72d8
PakiRV <66673187+PakiRV@users.noreply.github.com>
2021-12-26 21:02:28 +0100

    Update app.php

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Pau Egea Cortes'),
+    'name' => env('APP_NAME', 'The Binaries'),
 
     /*
     |--------------------------------------------------------------------------
