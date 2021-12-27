a763163022a487cb45a243018d0f913042dfe57d
Asier <aurunuelas@uoc.edu>
2021-12-27 20:52:07 +0100

    Entorno y configuración mail.php operativa

--- config/mail.php
+++ config/mail.php
@@ -51,6 +51,13 @@ return [
 
         'mailgun' => [
             'transport' => 'mailgun',
+            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+            'port' => env('MAIL_PORT', 25),
+            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'username' => env('MAIL_USERNAME'),
+            'password' => env('MAIL_PASSWORD'),
+            'timeout' => null,
+            'auth_mode' => null,
         ],
 
         'postmark' => [
